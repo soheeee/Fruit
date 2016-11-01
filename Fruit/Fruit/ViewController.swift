6c2c925092cbaf3b40d8cc6e5b3ab3019c7ae058
sohee <sohee@sohuiui-MacBook-Pro.local>
2016-11-01 15:02:43 +0900

    Initial Setting

---
+++ Fruit/Fruit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fruit
+//
+//  Created by 소희 on 2016. 11. 1..
+//  Copyright © 2016년 소희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
