95cc15a1fdb56d159e41fb592f982d71bf8e1436
sohee <sohee@sohuiui-MacBook-Pro.local>
2016-11-19 14:59:35 +0900

    AddAssignmentVC + AddExamVC

---
+++ Fruit/Fruit/AddExamViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddExamViewController.swift
+//  Fruit
+//
+//  Created by 소희 on 2016. 11. 19..
+//  Copyright © 2016년 소희. All rights reserved.
+//
+
+import UIKit
+
+class AddExamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setBackgroundColor()
+    }
+    
+    func setBackgroundColor(){
+        
+        let gradient = CAGradientLayer()
+        let blushTwo:CGColor = UIColor(red:0.96, green:0.57, blue:0.57, alpha:1.0).cgColor
+        let palePeach:CGColor = UIColor(red:1.0, green:0.90, blue:192.0/255.0, alpha:1.0).cgColor
+        gradient.colors = [blushTwo, palePeach]
+        gradient.frame = view.bounds
+        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Fruit/Fruit/MainNaviController.swift
+++ Fruit/Fruit/MainNaviController.swift
@@ -62,15 +62,16 @@ class MainNaviController : UINavigationController {
         let fab = KCFloatingActionButton(image: UIImage(named: "error")!)
         
         fab.addItem("과제추가", icon:UIImage(named:"assignment")!, color: UIColor(red: CGFloat(250)/255, green: CGFloat(190)/255, blue: CGFloat(170)/255, alpha: 1.0),handler: {item in
-            let alert = UIAlertController(title: "과제추가", message: "과제를 추가한다.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "네 알겠습니다", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "AddAssignment") as! AddAssignmentViewController
+            self.present(vc, animated: true, completion: nil)
             fab.close()
         })
+        
         fab.addItem("시험추가", icon:UIImage(named:"exam")!, color: UIColor(red: CGFloat(245)/255, green: CGFloat(147)/255, blue: CGFloat(147)/255, alpha: 1.0),  handler: {item in
-            let alert = UIAlertController(title: "시험추가", message: "시험을 추가한다.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "네 알겠습니다", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "AddExam") as! AddExamViewController
+            self.present(vc, animated: true, completion: nil)
             fab.close()
         })
         
