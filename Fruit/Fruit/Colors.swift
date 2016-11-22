60f93f4975e083eb908a3a865b48af6c958dfba0
sohee <sohee@sohuiui-MacBook-Pro.local>
2016-11-22 15:22:33 +0900

    color as global variable

---
+++ Fruit/Fruit/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  Fruit
+//
+//  Created by 소희 on 2016. 11. 22..
+//  Copyright © 2016년 소희. All rights reserved.
+//
+
+import Foundation
+
+let paleSalmon = UIColor(red: CGFloat(250)/255, green: CGFloat(190)/255, blue: CGFloat(170)/255, alpha: 1.0)
+let lightPeach = UIColor(red: CGFloat(254)/255, green: CGFloat(222)/255, blue: CGFloat(187)/255, alpha: 1.0)
+let blush = UIColor(red: CGFloat(248)/255, green: CGFloat(175)/255, blue: CGFloat(162)/255, alpha: 1.0)
+let blushTwo = UIColor(red: CGFloat(245)/255, green: CGFloat(147)/255, blue: CGFloat(147)/255, alpha: 1.0)
+let warmGrey = UIColor(red: CGFloat(160)/255, green: CGFloat(160)/255, blue: CGFloat(160)/255, alpha: 1.0)
+let brownishGrey = UIColor(red: CGFloat(100)/255, green: CGFloat(98)/255, blue: CGFloat(98)/255, alpha: 1.0)
+let redPink = UIColor(red: CGFloat(238)/255, green: CGFloat(65)/255, blue: CGFloat(86)/255, alpha: 1.0)
+let palePeach = UIColor(red: CGFloat(255)/255, green: CGFloat(231)/255, blue: CGFloat(192)/255, alpha: 1.0)
+let redPink50 = UIColor(red: CGFloat(238)/255, green: CGFloat(65)/255, blue: CGFloat(86)/255, alpha: 0.5)
+let white = UIColor(red: CGFloat(245)/255, green: CGFloat(245)/255, blue: CGFloat(245)/255, alpha: 1.0)
+
