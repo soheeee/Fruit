610a06f96d6e5441f92d03da5cf8e6e60d97fbdc
ohso <ohso@ohsoui-MacBook-Pro.local>
2016-12-07 14:26:41 +0900

    editing push alert

--- Fruit/Fruit/MainTableViewController.swift
+++ Fruit/Fruit/MainTableViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class MainTableViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
-    
     var loadTheme: Bool = {
         var theme = Theme.defaults.string(forKey: "Theme")
         if (theme == nil) {theme = "Peach"}
         Theme.loadTheme(name: theme!)
         return true
     }()
+    var settingPushAlert: Bool = {
+        var timeSetting = PushAlert.alertDefault.string(forKey: "PushTime")
+        if(timeSetting == nil) {
+            PushAlert.alertDefault.set(2, forKey: "PushTime")
+        }
+        var pushSetting = PushAlert.alertDefault.string(forKey: "PushEnabled")
+        if(pushSetting == nil){
+            PushAlert.alertDefault.set(true, forKey: "PushEnabled")
+        }
+        return true
+    }()
+    
     var startLocation:CGPoint!
     var lastLocation:CGPoint!
     var arrayItem:[Item] = []
@@ -382,4 +393,9 @@ class MainTableViewController: UIViewController,UITableViewDelegate,UITableViewD
         
     }
     
+}
+
+
+struct PushAlert {
+    static var alertDefault = UserDefaults.standard
 }
